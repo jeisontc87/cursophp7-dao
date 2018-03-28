b907fd977e8b07732c7d3b83a2c916f8d411b200
jeisontc87 <jeisontc87@gmail.com>
2018-03-28 03:39:03 -0300

    metodos de insert

--- index.php
+++ index.php
@@ -6,9 +6,27 @@
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	echo json_encode($usuarios);*/
-	$root = new Usuario();
-	$root->loadById(3);
-	echo $root;
 
+
+	//carrega um usuario
+	//$root = new Usuario();
+	//$root->loadById(3);
+	//echo $root;
+
+	//carrega uma lista de usuarios
+	//$lista = Usuario::getlist();
+	//echo json_encode($lista);
+
+
+	//carrega uma lista de usuarios buscando pelo login
+
+	//$search= Usuario::search("je");
+	//echo json_encode($search);
+
+	//carrega um usuario usando um login e a senha
+
+	$usuario = new Usuario();
+	$usuario->login("longdong","31313131");
+	echo $usuario;
 
  ?>
