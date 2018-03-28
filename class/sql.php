48e1ad83744d9b59a18115efceb44bd3ee9b40bb
jeisontc87 <jeisontc87@gmail.com>
2018-03-28 03:04:49 -0300

    criamos a classe usuario

--- class/sql.php
+++ class/sql.php
@@ -8,17 +8,17 @@
 			$this->conn = new PDO("mysql:host=localhost; dbname=dbphp7","root","");
 		} 
 
-		private function setParams($statment ,$parameters= array()){
+		private function setParams($statement ,$parameters= array()){
 
 			foreach ($parameters as $key => $value) {
 				
-				$this->setParam($key, $value);
+				$this->setParam($statement, $key, $value);
 
 			}
 		}
 
-		private function setParam($statmant, $key, $value){
-			$statmant->bindParam($key, $value);
+		private function setParam($statement, $key, $value){
+			$statement->bindParam($key, $value);
 		}
 
 		public function query($rawQuery, $params = array()){
